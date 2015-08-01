324765885027bd96f1f5805b689f2d15381f96e3
Jesse Hon <honkalong@gmail.com>
2015-08-01 17:39:42 +1000

    Remaining MakeDetail and ModelDetail views

--- lib/redbubble/views/make_detail_view.rb
+++ lib/redbubble/views/make_detail_view.rb
@@ -1,6 +1,38 @@
+require 'redbubble/views/base_view'
+
 module Redbubble
   module Views
-    class MakeDetailView < BaseDetailView
+    class MakeDetailView < BaseView
+      def initialize(template:, path_resolver:, make:)
+        super(template: template, path_resolver: path_resolver)
+        @make = make
+      end
+
+      def title
+        @make.name
+      end
+
+      def path
+        @path_resolver.absolute_make_detail(make_name: @make.name)
+      end
+
+      def back_links
+        [Link.new(name: 'Back to home', href: @path_resolver.absolute_index)]
+      end
+
+      def item_links
+        @make.models.each do |model|
+          href = @path_resolver.absolute_model_detail(make_name: @make.name, model_name: @model.name)
+          result << Link.new(name: model.name, href: href)
+        end
+      end
+
+      def thumbnails
+        result = []
+        @model.works.first(10).each do |work|
+          result << Image.new(src: work.url)
+        end
+      end
     end
   end
 end
--- lib/redbubble/views/model_detail_view.rb
+++ lib/redbubble/views/model_detail_view.rb
@@ -1,6 +1,33 @@
+require 'redbubble/views/base_view'
+
 module Redbubble
   module Views
-    class ModelDetailView < BaseDetailView
+    class ModelDetailView < BaseView
+      def initialize(template:, path_resolver:, model:)
+        super(template: template, path_resolver: path_resolver)
+        @model = model
+      end
+
+      def title
+        "%s - %s" % (@model.make_name, @model.name)
+      end
+
+      def path
+        @path_resolver.absolute_model_detail(make_name: @model.make_name, model_name: @model.name)
+      end
+
+      def back_links
+        [ Link.new(name: 'Back to home', href: @path_resolver.absolute_index),
+          Link.new(name: 'Back to make', href: @path_resolver.absolute_make_detail(make_name: @model.make_name)),
+        ]
+      end
+
+      def thumbnails
+        result = []
+        @model.works.first(10).each do |work|
+          result << Image.new(src: work.url)
+        end
+      end
     end
   end
 end
