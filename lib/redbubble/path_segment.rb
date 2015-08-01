efdad099c1d02711841c7e1734e35748458c9e29
Jesse Hon <honkalong@gmail.com>
2015-08-02 02:44:10 +1000

    Fix not class methods

--- lib/redbubble/path_segment.rb
+++ lib/redbubble/path_segment.rb
@@ -2,20 +2,20 @@ module Redbubble
   class PathSegment
     attr_reader :title, :path;
 
-    def initialize(title:, path)
+    def initialize(title:, path: nil)
       @title = title
       @path = (defined?(path))? path : slugify(title)
     end
 
-    def create_for_index
+    def self.create_for_index
       PathSegment.new(title: "Redbubble")
     end
 
-    def create_for_make(make_name:)
+    def self.create_for_make(make_name:)
       PathSegment.new(title: make_name)
     end
 
-    def create_for_model(model_name:)
+    def self.create_for_model(model_name:)
       PathSegment.new(title: model_name)
     end
 
