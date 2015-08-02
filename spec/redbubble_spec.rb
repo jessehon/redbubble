97ae163cae2116d3624d73d90c4ba3635a959abb
Jesse Hon <honkalong@gmail.com>
2015-08-03 03:04:38 +1000

    Extend application test case to check index html as well

--- spec/redbubble_spec.rb
+++ spec/redbubble_spec.rb
@@ -15,14 +15,41 @@ describe Redbubble do
         Redbubble.run(input_file: input_file, output_path: @output_path)
       end
 
-      it 'created file for index' do
+      it 'created file' do
         file_path = File.join(@output_path, "index.html")
         expect(File.exist?(file_path)).to eq(true)
       end
 
-      it 'created file for index' do
-        file_path = File.join(@output_path, "index.html")
-        expect(File.exist?(file_path)).to eq(true)
+      describe "index html" do
+        let(:result) { File.read(File.join(@output_path, "index.html")) }
+
+        it 'has title' do
+          expected = "<title>Redbubble</title>"
+          expect(result).to include(expected)
+        end
+
+        it 'has links' do
+          expected_links = [
+            {href: "nikon-corporation/index.html", name: "NIKON CORPORATION"},
+            {href: "canon/index.html", name: "Canon"},
+          ]
+          expected_links.each do |link|
+            link_path = File.join(@output_path, link[:href])
+            expected_content = "<a href=\"#{link_path}\">#{link[:name]}</a>"
+            expect(result).to include(expected_content)
+          end
+        end
+
+        it 'has thumbnails' do
+          expected_thumbail_urls = [
+            "http://ih1.redbubble.net/work.31820.1.flat,135x135,075,f.jpg",
+            "http://ih1.redbubble.net/work.2041.1.flat,135x135,075,f.jpg"
+          ]
+          expected_thumbail_urls.each do |url|
+            expected = "<img src=\"#{url}\">"
+            expect(result).to include(expected)
+          end
+        end
       end
 
       it 'created file for makes' do
